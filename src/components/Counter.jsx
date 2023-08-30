e0b007cea49d7e712d63c04c313511c10a00dcbc
Semetery29 <mdazizulhakimsuny@gmail.com>
2023-08-31 02:53:50 +0600

    Redux First module done.

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,7 +1,25 @@
+import { useDispatch, useSelector } from "react-redux";
+import {
+  decrement,
+  increment,
+  incrementByValue,
+} from "../redux/features/counter/counterSlice";
+
 const Counter = () => {
+  const { count } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  //  note dispatch action should be called with the function.
   return (
     <div>
-      <h1>Counter</h1>
+      <button onClick={() => dispatch(incrementByValue(5))}>
+        Increment by 5
+      </button>
+      <br />
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <div>
+        <h1>{count}</h1>
+      </div>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
     </div>
   );
 };
--- src/redux/features/counter/counterSlice.js
+++ src/redux/features/counter/counterSlice.js
@@ -8,15 +8,18 @@ const counterSlice = createSlice({
   name: "counter",
   initialState,
   reducers: {
-    increment : (state) => {
-        state.count = state.count + 1;
+    increment: (state) => {
+      state.count = state.count + 1;
     },
-    decrement : (state) => {
-        state.count = state.count - 1;
-    }
+    decrement: (state) => {
+      state.count = state.count - 1;
+    },
+    incrementByValue: (state, actions) => {
+      state.count = state.count + actions.payload;
+    },
   },
 });
 
-export const {increment, decrement} = counterSlice.actions;
+export const { increment, decrement, incrementByValue } = counterSlice.actions;
 
 export default counterSlice.reducer;
